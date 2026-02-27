c74378a3ef318b53699b3162ac6afc00fd1f35e3
Jade Geels <jade@justbetter.nl>
2026-02-27 13:07:47 +0000

    Rapidez v5 support (#2)

--- resources/js/package.js
+++ resources/js/package.js
@@ -1,1 +1,6 @@
-Vue.component('reorder', () => import('./components/Reorder.vue'))
+import { defineAsyncComponent } from 'vue'
+
+document.addEventListener('vue:loaded', function (event) {
+    const vue = event.detail.vue
+    vue.component('reorder', defineAsyncComponent(() => import('./components/Reorder.vue')))
+})
