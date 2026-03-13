3f42934b05a688d13066c4faf910e84dd01e9998
Vincent Boon <vincent@justbetter.nl>
2026-03-13 11:32:40 +0100

    Ran AI review and fixed any issues found

--- resources/views/components/add-to-cart.blade.php
+++ resources/views/components/add-to-cart.blade.php
@@ -1,6 +1,6 @@
 @props(['component' => 'rapidez::button.conversion'])
 
-<div {{ $attributes->class('border-t flex p-2 sticky bottom-0 bg-white items-center') }}>
+<div {{ $attributes->class('border-t border-gray-200 flex p-2 sticky bottom-0 bg-white items-center') }}>
     <div>
         <template v-if="reorderSlotScope.selectedItems.length == 1">
             @lang(':count product selected', ['count' => '@{{ reorderSlotScope.selectedItems.length }}'])
